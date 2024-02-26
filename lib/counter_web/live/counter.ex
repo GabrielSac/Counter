a2723c1dc14bf5f225945a6c31bf2b8448e8f891
Gabriel.Sac <gabrielsachimelfarb@gmail.com>
2024-02-26 17:50:14 -0300

    Initial commit

---
+++ lib/counter_web/live/counter.ex
@@ -0,0 +1,38 @@
+defmodule CounterWeb.Counter do
+  use CounterWeb, :live_view
+
+  @topic "live"
+  def mount(_params, _session, socket) do
+    CounterWeb.Endpoint.subscribe(@topic)
+    {:ok, assign(socket, :val, 0)}
+  end
+
+  def handle_event("inc", _, socket) do
+    new_state = update(socket, :val, &(&1 + 1))
+    CounterWeb.Endpoint.broadcast_from(self(), @topic, "inc", new_state.assigns)
+    {:noreply, new_state}
+  end
+
+  def handle_event("dec", _, socket) do
+    new_state = update(socket, :val, &(&1 - 1))
+    CounterWeb.Endpoint.broadcast_from(self(), @topic, "dec", new_state.assigns)
+    {:noreply, new_state}
+  end
+
+  def handle_info(msg, socket) do
+    {:noreply, assign(socket, val: msg.payload.val)}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <h1 class="text-4xl font-bold text-center">The count is: <%= @val %></h1>
+
+      <p class="text-center">
+        <.button phx-click="dec">-</.button>
+        <.button phx-click="inc">+</.button>
+      </p>
+    </div>
+    """
+  end
+end
